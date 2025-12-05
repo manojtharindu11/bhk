898c93fb823d650070c803579c874f60308d8fe8
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2025-12-05 22:39:37 +0530

    Add Flask server and utility functions for home price prediction
    - Implemented Flask server in `server/app.py` with endpoints for fetching location names and predicting home prices.
    - Created utility functions in `server/util.py` to handle model loading and price estimation logic.
    - Added model artifact `bhp_model.pickle` and column metadata `columns.json` to the output directory.

---
+++ server/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+import util
+
+app = Flask(__name__)
+
+@app.route("/get-location-names")
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    }), 200
+    
+    return response
+
+@app.route("/predict-home-price", methods=["POST"])
+def predict_home_price():
+    total_sqft = float(request.form["total_sqft"])
+    location = request.form["location"]
+    bhk = request.form["bhk"]
+    bath = request.form["bath"]
+    
+    response = jsonify({
+        "estimated_price" : util.get_estimated_price(location, total_sqft, bhk, bath)
+    }), 200
+    
+    return response
+    
+
+if __name__ == "__main__":
+    print("Starting flask server....")
+    util.load_saved_artifacts()
+    app.run(debug=True)
